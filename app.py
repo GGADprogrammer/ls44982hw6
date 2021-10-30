74e2d4024ed09b290e28284390d93d6b8480c15c
Litong Song <songlitong@bytedance.com>
2021-10-30 14:52:30 -0500

    flask module

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def serve():
     return send_from_directory(app.static_folder, 'index.html')
 if __name__ == '__main__':
     #app.run(host='0.0.0.0')
-    app.run(host='0.0.0.0',debug=True, port=33507)
+    app.run(host='0.0.0.0',debug=True, port=5000)
