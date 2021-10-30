f0699e42efedeebbe9a623647b9705889c0cd9dc
Litong Song <songlitong@bytedance.com>
2021-10-30 13:22:11 -0500

    frontend

--- app.py
+++ app.py
@@ -15,4 +15,5 @@ def Welcome():
 def serve():
     return send_from_directory(app.static_folder, 'index.html')
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True, port=33507)
